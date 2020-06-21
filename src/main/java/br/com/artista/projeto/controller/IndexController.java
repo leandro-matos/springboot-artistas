b8e69ef9d775f326f0dbfb499bfba96a4d1d225f
leandro_matos <59984034+leandro-matos@users.noreply.github.com>
2020-06-21 17:33:30 -0300

    Alterando o package Controller

---
+++ src/main/java/br/com/artista/projeto/controller/IndexController.java
@@ -0,0 +1,16 @@
+package br.com.artista.projeto.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class IndexController {
+
+	@GetMapping
+	public String get() {
+		return "API com a Lista de Artistas/Músicas";
+	}
+	
+}
