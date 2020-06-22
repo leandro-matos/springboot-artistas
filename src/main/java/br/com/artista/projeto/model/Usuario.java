06bfd2e7bec73a88162cbd9909c467983b2cb444
leandro_matos <59984034+leandro-matos@users.noreply.github.com>
2020-06-21 22:10:55 -0300

    make it better

--- src/main/java/br/com/artista/projeto/model/Usuario.java
+++ src/main/java/br/com/artista/projeto/model/Usuario.java
@@ -28,49 +28,96 @@ public class Usuario {
 	@Column(name="foto", length=200, nullable=false)
 	private String foto;
 	
-	public Usuario() {
-		super();
-	}
+	@Column(name="telefone", length=15, nullable=false)
+	private String telefone;
 	
-	public Usuario(int id, String nome, String email, String senha, String foto) {
-		super();
-		this.id = id;
-		this.nome = nome;
-		this.email = email;
-		this.senha = senha;
-		this.foto = foto;
-	}
+	@Column(name="endereco", length=200, nullable=false)
+	private String endereco;
+	
+	@Column(name="idade", nullable=false)
+	private int idade;
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getSenha() {
 		return senha;
 	}
+
 	public void setSenha(String senha) {
 		this.senha = senha;
 	}
+
 	public String getFoto() {
 		return foto;
 	}
+
 	public void setFoto(String foto) {
 		this.foto = foto;
 	}
-	
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	public Usuario(int id, String nome, String email, String senha, String foto, String telefone, String endereco,
+			int idade) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.email = email;
+		this.senha = senha;
+		this.foto = foto;
+		this.telefone = telefone;
+		this.endereco = endereco;
+		this.idade = idade;
+	}
+
+	public Usuario() {
+		super();
+	}
+
 	
 	
 }
